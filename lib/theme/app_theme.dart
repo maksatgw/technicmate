318930c75d80de6fc5a4a877749110ecd3a89616
maksatgw <garyagdyyev@outlook.com>
2024-05-19 12:47:22 +0300

    first commit

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:technicmate/theme/palette.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData.dark().copyWith(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Palette.black,
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: Palette.black,
+    ),
+    tabBarTheme: TabBarTheme(
+      labelColor: Palette.white,
+      labelStyle: GoogleFonts.cabin(fontSize: 20),
+      unselectedLabelStyle: GoogleFonts.cabin(fontSize: 20),
+      indicator: const UnderlineTabIndicator(
+        borderSide: BorderSide(color: Palette.white),
+      ),
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Palette.black,
+      elevation: 0,
+    ),
+  );
+}
---
+++ lib/theme/palette.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const transparent = Colors.transparent;
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const authTextFieldFillColor = Color(0xFF00182B);
+  static const authSuffixColor = Color(0xFF005493);
+
+  static const usernameGrey = Color(0xff999999);
+  static const seperatorGrey = Color(0xff1D1D1D);
+
+  static const loginButtonBlueColor = Color(0xFF4C9EEB);
+  static const loginButtonDarkBlueColor = Color(0xFF0B253E);
+
+  static const searchFieldFillColor = Color(0xFF141414);
+  static const textGrey777 = Color(0xFF777777);
+  static const textGrey9999 = Color(0xFF999999);
+  static const textGreyA1A1A1 = Color(0xFFA1A1A1);
+
+  static const chatTextFieldBg = Color(0xFF141414);
+  static const chatDarkBlueButtonColor = Color(0xFF052631);
+  static const chatBlueButtonColor = Color(0xFF00C2FF);
+  static const chatBubbleDarkBlueColor = Color(0xFF041C24);
+
+  static const tmpostCreateChipInactive = Color(0xFF6D6D6D);
+}
