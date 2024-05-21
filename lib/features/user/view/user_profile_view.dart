fe6e6837001110940f6c8764569ba8e82f62ef25
maksatgw <garyagdyyev@outlook.com>
2024-05-21 15:43:39 +0300

    User Profile Fixes

--- lib/features/user/view/user_profile_view.dart
+++ lib/features/user/view/user_profile_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
 import 'package:technicmate/constants/asset_constants.dart';
 import 'package:technicmate/features/user/controller/user_profile_controller.dart';
 import 'package:technicmate/theme/theme.dart';
@@ -24,35 +23,33 @@ class UserProfileView extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 CircleAvatar(
-                  backgroundColor: Palette.chatBlueButtonColor,
-                  backgroundImage: NetworkImage("${controller.userModel.value.data?.profileImageData.toString()}"),
                   radius: 40,
+                  backgroundImage: NetworkImage(
+                    controller.userModel.value.data?.profileImageData ?? AssetConstants.defaultProfileImage,
+                  ),
                 ),
-                UserProfileCustomStatic(count: controller.userModel.value.data?.noteCount?.toInt(), title: "Not"),
-                UserProfileCustomStatic(count: controller.userModel.value.data?.followerCount, title: "Takipçi"),
-                UserProfileCustomStatic(count: controller.userModel.value.data?.followedByCount, title: "Takip"),
+                UserProfileCustomStatic(count: controller.userModel.value.data?.noteCount ?? 0, title: "Not"),
+                UserProfileCustomStatic(count: controller.userModel.value.data?.followerCount ?? 0, title: "Takipçi"),
+                UserProfileCustomStatic(count: controller.userModel.value.data?.followedByCount ?? 0, title: "Takip"),
               ],
             ),
             const SizedBox(height: 10),
-            Text("${controller.userModel.value.data?.firstname} ${controller.userModel.value.data?.lastname}",
-                style: GoogleFonts.inter(fontSize: 20, fontWeight: FontWeight.bold, color: Palette.white)),
+            Text(
+              "${controller.userModel.value.data?.firstname} ${controller.userModel.value.data?.lastname}",
+              style: GoogleFonts.inter(fontSize: 20, fontWeight: FontWeight.bold, color: Palette.white),
+            ),
+            Text(
+              controller.userModel.value.data?.university?.title ?? "",
+              style: GoogleFonts.inter(fontSize: 13, fontWeight: FontWeight.w300, color: Palette.),
+            ),
             const SizedBox(height: 10),
-            const UserProfileCustomClassNumberIndicator(),
+            UserProfileCustomClassNumberIndicator(department: controller.userModel.value.data?.department?.title ?? ""),
             const SizedBox(height: 10),
             Text(
-              "Here to change.",
+              controller.userModel.value.data?.biography ?? "",
               style: GoogleFonts.inter(color: Palette.textGrey9999),
             ),
             const SizedBox(height: 10),
-            // Center(
-            //   child: ElevatedButton(
-            //     onPressed: () {},
-            //     style: ElevatedButton.styleFrom(
-            //       minimumSize: Size(MediaQuery.of(context).size.width, 35),
-            //     ),
-            //     child: Text("Takip Et", style: GoogleFonts.inter(fontSize: 16, color: Palette.white)),
-            //   ),
-            // ),
             const SizedBox(height: 5),
             TabBar(
               controller: controller.tabController,
@@ -209,7 +206,10 @@ Widget getIconForPostType(int? postTypeId) {
 class UserProfileCustomClassNumberIndicator extends StatelessWidget {
   const UserProfileCustomClassNumberIndicator({
     super.key,
+    required this.department,
   });
+
+  final String department;
 
   @override
   Widget build(BuildContext context) {
@@ -228,14 +228,8 @@ class UserProfileCustomClassNumberIndicator extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
             child: Text.rich(
               TextSpan(
-                text: 'Fizik ',
-                style: GoogleFonts.inter(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black),
-                children: <InlineSpan>[
-                  TextSpan(
-                    text: '1/4',
-                    style: GoogleFonts.inter(fontSize: 15, fontWeight: FontWeight.bold, color: Palette.white),
-                  ),
-                ],
+                text: '${department} ',
+                style: GoogleFonts.inter(fontSize: 15, fontWeight: FontWeight.bold, color: Palette.loginButtonDarkBlueColor),
               ),
             ),
           ),
@@ -254,12 +248,11 @@ class UserProfileCustomStatic extends StatelessWidget {
   });
   final int? count;
   final String title;
-  final formatter = NumberFormat('#,##,000');
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(formatter.format(count), style: GoogleFonts.inter(fontSize: 15, color: Palette.white)),
+        Text(count.toString(), style: GoogleFonts.inter(fontSize: 15, color: Palette.white)),
         Text(title, style: GoogleFonts.inter(fontSize: 15, color: Palette.white)),
       ],
     );
